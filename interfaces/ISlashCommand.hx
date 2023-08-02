63e782a66249a426bf25bd6682310bd15daba151
BCT2006 <bctix2006@gmail.com>
2023-08-01 17:56:02 -0700

    cant use static vars for interface

--- interfaces/ISlashCommand.hx
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-import hxdiscord.types.structTypes.ApplicationCommand.ApplicationCommandOption;
-
-interface ISlashCommand {
-    public var description:String;
-    public var type:Int;
-    public var options:Array<ApplicationCommandOption>;
-}
